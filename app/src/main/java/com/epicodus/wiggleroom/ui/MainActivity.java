bbb8525dea6cb297e3dae74791e1db803216ad23
Daren Schaad and Matt Rosanio <mmrosani@syr.edu>
2016-05-09 11:27:52 -0700

    Set up main activity with gestureDetector that registers onDown and onFling gestures on text view

---
+++ app/src/main/java/com/epicodus/wiggleroom/ui/MainActivity.java
@@ -0,0 +1,57 @@
+package com.epicodus.wiggleroom.ui;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.epicodus.wiggleroom.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    GestureDetector gestureDetector;
+    @Bind(R.id.textView)
+    TextView mTextView;
+    private GestureDetectorCompat mDetector;
+    private static final String DEBUG_TAG = "Gestures";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetectorCompat(this,new MyGestureListener());
+        mTextView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                mDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG,"onDown: " + event.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Log.d(DEBUG_TAG, "onFling: " + event1.toString()+event2.toString());
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/epicodus/wiggleroom/util/OnDoubleClickListener.java
@@ -0,0 +1,8 @@
+package com.epicodus.wiggleroom.util;
+
+/**
+ * Created by Guest on 5/9/16.
+ */
+public interface OnDoubleClickListener {
+
+}
