45b386edf10033428b6e1f5a3b08fa5d4fe5a59d
Daren Schaad and Matt Rosanio <mmrosani@syr.edu>
2016-05-09 14:39:08 -0700

    commit so abby can steal our stuff

--- app/src/main/java/com/epicodus/wiggleroom/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/wiggleroom/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.wiggleroom.ui;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.epicodus.wiggleroom.R;
@@ -19,9 +21,12 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     GestureDetector gestureDetector;
-    @Bind(R.id.textView)
-    TextView mTextView;
-    private GestureDetectorCompat mDetector;
+    @Bind(R.id.textView) TextView mTextView;
+    @Bind(R.id.imageView) ImageView mImageView;
+    private GestureDetectorCompat mTextViewDetector;
+    private GestureDetectorCompat mImageViewDetector;
+    Context mContext;
+
     private static final String DEBUG_TAG = "Gestures";
 
     @Override
@@ -29,19 +34,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mContext = this;
         // Instantiate the gesture detector with the
         // application context and an implementation of
         // GestureDetector.OnGestureListener
-        mDetector = new GestureDetectorCompat(this,new MyGestureListener());
+        mTextViewDetector = new GestureDetectorCompat(this,new TextViewGestureListener());
         mTextView.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent event) {
-                mDetector.onTouchEvent(event);
+                mTextViewDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+        mImageViewDetector = new GestureDetectorCompat(this, new ImageViewGestureListener());
+        mImageView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                mImageViewDetector.onTouchEvent(event);
                 return true;
             }
         });
     }
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+    class TextViewGestureListener extends GestureDetector.SimpleOnGestureListener {
         private static final String DEBUG_TAG = "Gestures";
 
         @Override
@@ -65,9 +78,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onDoubleTap(MotionEvent event) {
-            Animation doubleTapAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.double_tap_animation);
+            Animation doubleTapAnimation = AnimationUtils.loadAnimation(mContext, R.anim.double_tap_animation);
             mTextView.startAnimation(doubleTapAnimation);
             return true;
         }
     }
+
+    class ImageViewGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "ImageGestures";
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Animation flingAnimation = AnimationUtils.loadAnimation(mContext, R.anim.boomerang_fling_animation);
+            mImageView.startAnimation(flingAnimation);
+//            mImageView.animate().scaleX(0.0f).scaleY(0.0f).setDuration(500);
+//            mImageView.animate().scaleX(1f).scaleY(1f).setDuration(500);
+
+            Log.d(DEBUG_TAG, "onFling: " + event1.toString()+event2.toString());
+            return true;
+        }
+    }
+
+
 }
